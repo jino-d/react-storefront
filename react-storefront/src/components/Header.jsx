3d40300cc626788592e0e3ce4796451ff55686bf
jino-d <jino.c.dologmanding@gmail.com>
2023-01-01 22:12:41 +0800

    Added configs. Added login page.

---
+++ react-storefront/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { BiLogIn } from "react-icons/bi"
+
+export default function Header(params) {
+   return (
+        <div className="h-[100px] bg-rose-500 shadow-lg flow-root">
+            <div className="flex">
+                <div className="text-xl font-bold p-7 text-white">
+                    My App
+                </div>
+                <div className="text-xl font-bold p-7 text-white absolute right-0 flex items-center cursor-pointer hover:text-rose-200">
+                    <p className="pr-2">Login</p>
+                    <BiLogIn className=""/>
+                </div>
+            </div>
+        </div>
+   )
+}
